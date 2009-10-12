97a300e6c9dfabf6d54f537284d27110c2faa6ef
Simon Twigger <simont@mcw.edu>
2009-10-11 23:19:39 -0500

    bug fixes to parse biomart output correctly

---
+++ Affymetrix/biomartAffytoRDF.rb
@@ -0,0 +1,26 @@
+#! /opt/local/bin/ruby
+
+
+
+file_name = ARGV[0] || "../spec/files/biomartAffytoRDF_test.txt"
+chip_set = ARGV[1] || "rae230b"
+
+File.open(file_name,"r") do |file|
+
+  while (line = file.gets)
+
+    line.strip!
+    if /^$/ !~ line
+    # puts "Line: #{line}"
+    results = line.split("\t")
+
+    next if results[0] == "RGD ID"
+    
+      if !results[0].empty? && (results[1] != nil)
+        # puts "rgd: #{results[0]}. probe: #{results[1]}."
+        puts "<http://bio2rdf.org/rgd:#{results[0]}> <http://bio2rdf.org/ns/bio2rdf#xAffymetrix> <http://bio2rdf.org/ns/affymetrix:#{results[1]}> ."
+        puts "<http://bio2rdf.org/affymetrix:#{results[0]}> <http://bio2rdf.org/ns/bio2rdf#partOf> <http://bio2rdf.org/ns/affymetrix:#{chip_set}> ."
+      end
+    end
+  end
+end
